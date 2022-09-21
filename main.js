4ebc4340cfc7f0d03d0b231220ddac27e366a001
YennyMarcela MárquezCorreaADSO <yennycorrea0911@gmail.com>
2022-09-21 11:20:53 -0500

    Ejercicio 12

--- main.js
+++ main.js
@@ -1,35 +1,18 @@
 addEventListener("DOMContentLoaded", ()=>{
-    // Ejercicio 11. 
-//Programa para solicitar el nombre, apellido, edad y la nota promedio de 5 estudiantes de un curso de computación.
+    // Ejercicio 12 
+//Restarle al primer número al segundo (siempre y cuando el primero sea mayor que él segundo, en caso contrario indicar con un mensaje que la operación no es posible realizarla.
 
-    let estudiante1 = (prompt("Ingrese su nombre completo: "))
-    let edad1 = Number(prompt("Ingrese su edad: "))
-    let nota1 = Number(prompt("Ingrese su nota:"))
-
-    let estudiante2 = (prompt("Ingrese su nombre completo: "))
-    let edad2 = Number(prompt("Ingrese su edad: "))
-    let nota2 = Number(prompt("Ingrese su nota:"))
-
-    let estudiante3 = (prompt("Ingrese su nombre completo: "))
-    let edad3 = Number(prompt("Ingrese su edad: "))
-    let nota3 = Number(prompt("Ingrese su nota:"))
-
-    let estudiante4 = (prompt("Ingrese su nombre completo: "))
-    let edad4 = Number(prompt("Ingrese su edad: "))
-    let nota4 = Number(prompt("Ingrese su nota:"))
-
-    let estudiante5 = (prompt("Ingrese su nombre completo: "))
-    let edad5 = Number(prompt("Ingrese su edad: "))
-    let nota5 = Number(prompt("Ingrese su nota:"))
+    let numero1 = Number(prompt("Ingrese el numero 1: "))
+    let numero2 = Number(prompt("Ingrese el numero 2: "))
   
-    Promedio = (nota1 + nota2 + nota3 + nota4 + nota5)/5
 
-    if (pro > 0 ){
+    if (numero1 > numero2){
         
-        console.log(`El promedio de los cinco estudiantes es: "${promedio}"`);
+        resta = numero1-numero2
+        console.log(`Total resta: "${promedio}"`);
 
     } else{
-        console.log(`Lo ingresado no es valido`);
+        console.log(`La resta no se puede realizaar`);
     }
     
 
