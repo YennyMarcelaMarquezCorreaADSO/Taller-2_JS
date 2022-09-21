a1badd35d3b172f071f76fc9b1ec116abe20469e
YennyMarcela MárquezCorreaADSO <yennycorrea0911@gmail.com>
2022-09-21 11:11:27 -0500

    Ejercicio 10

--- main.js
+++ main.js
@@ -1,50 +1,29 @@
 addEventListener("DOMContentLoaded", ()=>{
-    // Ejercicio 9. 
-//Realizar el algoritmo que lea N números, calcule y escriba la suma de los pares y el producto de los impares. Numero par es aquel que su residuo igual a cero (num mod !=0)
+    // Ejercicio 10. 
+//Calcular el valor total del valor de 5 productos, el IVA y el subtotal, visualizar los resultados de: Iva, Subtotal y Total de la compra de los artículos.
 
 
-    let pares, impares
+    let producto1 = Number(prompt("Ingrese el valor del produto 1: "))
+    let producto2 = Number(prompt("Ingrese el valor del  produto 2: "))
+    let producto3 = Number(prompt("Ingrese el valor del produto 3:"))
+    let producto4 = Number(prompt("Ingrese el valor del produto 4:"))
+    let producto5 = Number(prompt("Ingrese el valor del produto 5:"))
 
-    pares = []
-    impares = []
+    sumaprecios = producto1 + producto2 + producto3 + producto4 + producto5
 
-    cantidad = Number(prompt("Ingrese la cantidad de numeros que desea calcular"))
-    for (var y of cantidad){
-        numero = Number(prompt("Ingrese un número")) 
-        if (numero%2 != 0){
-            impares.append(numero)
-        }else if ( numero%2 == 0){
-            pares.append(numero)
-        }
+    if (sumaprecios > 0 ){
+        subtotal = producto1 + producto2 + producto3 + producto4 + producto5
 
-    Sumpares = sum(pares)
-    proimp = np.prod(impares)
+        console.log(` Sueldo de trabajador de planta según la cantidad de horas introducidas: ${subtotal}`);
 
-    console.log(`El resultado de la operación de los números pares es: "${sumpares}"`);
+        IVA = sumaprecios*19/100
+        console.log(` Sueldo de trabajador de planta según la cantidad de horas introducidas: ${IVA}`);
 
-    console.log(`El resultado de la operación de los números pares es: "${proImp}"`);
+        Totalcompra = sumaprecios + IVA
+        console.log(` Sueldo de trabajador de planta según la cantidad de horas introducidas: ${Totalcompra}`);
+    } else{
+        console.log(`Lo ingresado no es valido`);
     }
-
-
-       
     
 
-
-
-
-
-        console.log(`%cFormula ${horas} * ${pagahora} `, `background: rebeccapurple; color: antiquewhite; font-family: cursive; padding: 2px; border-radius: 1px solid #925fc4; border-radius: 5px;`);
-
-        console.log(` Sueldo de trabajador de planta según la cantidad de horas introducidas: ${sueldo}`);
-
-    } else if(Trabaja == "A" ){
-        horas = Number(prompt("¿Cantidad de horas trabajadas al día?"))
-        pagahora = 10000
-        sueldo = horas * pagahora
-
-        console.log(`%cFormula ${horas} * ${pagahora} `, `background: rebeccapurple; color: antiquewhite; font-family: cursive; padding: 2px; border-radius: 1px solid #925fc4; border-radius: 5px;`);
-
-        console.log(`Sueldo de trabajador Administrativo según la cantidad de horas introducidas: ${sueldo}`);
-
-    }
 })
