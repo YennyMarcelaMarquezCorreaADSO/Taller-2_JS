81645fc1602c41ba0e31e5902c8420226dd5a0dc
YennyMarcela MárquezCorreaADSO <yennycorrea0911@gmail.com>
2022-09-21 10:14:15 -0500

    Ejercicio 5

--- main.js
+++ main.js
@@ -1,26 +1,16 @@
 addEventListener("DOMContentLoaded", ()=>{
-    // Ejercicio 4. 
-//Cálculo de áreas - Elige una figura geométrica:" Triángulo y Círculo
-// ¿Qué figura quiere calcular (Escriba T o C)?
-// Triangulo = base * altura / 2
-// Circulo = PI * radio* radio
+    // Ejercicio 5. 
+//Escriba un algoritmo que calcule el área de un rectángulo siempre y cuando los lados sean positivos:
+// área triangulo= lado * lado
 
-    let figura = (prompt("El área de que figura desea calcular Triangulo(T) o Circulo(C): "))
+    let lado = Number(prompt("Ingrese el valor del lado: "))
 
-    if (figura ==  "T"){
-        base = Number(prompt("Ingrese el valor de la base: "))
-        altura = Number(prompt("Ingrese el valor de la altura: "))
-        area = (base * altura)/2
+    if (lado > 0 ){
 
-        console.log(`%cFormula (${base} * ${altura}) / 2`, `background: rebeccapurple; color: antiquewhite; font-family: cursive; padding: 2px; border-radius: 1px solid #925fc4; border-radius: 5px;`);
-        console.log(`El área del triángulo es igual a : "${area}"`);
+        area = lado * lado
 
-    }else if (figura == "C") {
-        radio = Number(prompt("Ingrese el valor del radio: "))
-        area = Math.PI * Math.pow(radio, 2)
-
-        console.log(`%cFormula ${Math.PI} * ${Math.pow(radio, 2)}`, `background: rebeccapurple; color: antiquewhite; font-family: cursive; padding: 2px; border-radius: 1px solid #925fc4; border-radius: 5px;`);
-        console.log(`El área del circulo es igual a : "${area}"`);
+        console.log(`%cFormula ${lado} * ${lado}`, `background: rebeccapurple; color: antiquewhite; font-family: cursive; padding: 2px; border-radius: 1px solid #925fc4; border-radius: 5px;`);
+        console.log(`El área del rectángulo es igual a : "${area}"`);
 
     }else{
         console.log("Lo ingresado no es valido")
