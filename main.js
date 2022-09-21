f8f2d7024db82183785b69dee0a9d3e79f7acdb0
YennyMarcela MárquezCorreaADSO <yennycorrea0911@gmail.com>
2022-09-21 11:29:50 -0500

    Ejercicio 13

--- main.js
+++ main.js
@@ -1,19 +1,32 @@
 addEventListener("DOMContentLoaded", ()=>{
-    // Ejercicio 12 
-//Restarle al primer número al segundo (siempre y cuando el primero sea mayor que él segundo, en caso contrario indicar con un mensaje que la operación no es posible realizarla.
+    // Ejercicio 13
+//En una tienda de HELADO da un descuento por compra a sus clientes con membresía dependiendode su tipo, sólo existen tres tipos de membresía, tipo A, tipo B y tipo C.
+// Los descuentos son los siguientes:
+// Tipo A 10% de descuento
+// Tipo B 15% de descuento
+// Tipo C 20% de descuento
 
-    let numero1 = Number(prompt("Ingrese el numero 1: "))
-    let numero2 = Number(prompt("Ingrese el numero 2: "))
-  
+    let membresia = (prompt("Ingrese a que tipo de membresia pertenece(A) (B) O (C): "))
 
-    if (numero1 > numero2){
-        
-        resta = numero1-numero2
-        console.log(`Total resta: "${promedio}"`);
+    if (membresia == "A"){
+        total = Number(prompt("Ingrese el total consumido"))
+        descu= total * 10/100
+        descuento = total - descu
 
-    } else{
-        console.log(`La resta no se puede realizaar`);
+        console.log(`Total con descuento realizado: "${descuento}"`);
+
+    }else if (membresia == "B"){
+        total = Number(prompt("Ingrese el total consumido"))
+        descu= total * 15/100
+        descuento = total - descu
+
+        console.log(`Total con descuento realizado: "${descuento}"`);
+
+    }else if (membresia == "C"){
+        total = Number(prompt("Ingrese el total consumido"))
+        descu= total * 20/100
+        descuento = total - descu
+
+        console.log(`Total con descuento realizado: "${descuento}"`);
     }
-    
-
 })
