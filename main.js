a96c13b72d43daa000dda4f5932c780b0be1f240
YennyMarcela MárquezCorreaADSO <yennycorrea0911@gmail.com>
2022-09-21 10:39:20 -0500

    Ejercicio 8

--- main.js
+++ main.js
@@ -1,18 +1,27 @@
 addEventListener("DOMContentLoaded", ()=>{
-    // Ejercicio 7. 
-//Calcular todos los pagos hechos de un restaurante y que si el consumo ingresado excede los $130.000 el descuento será del 15%, de lo contrario no hay descuento
+    // Ejercicio 8. 
+//En cierta empresa se les paga a sus trabajadores de la siguiente forma: si el empleado es de planta, la hora trabajada se le paga a $20000, si el empleado es administrativo, la hora trabajada se le paga a $10000. Para calcular su pago es necesario conocer el total de horas trabajadas.
 
-    let consumo = Number(prompt("Ingrese su total de consumo: "))
+    let Trabaja = (prompt("Ingrese en que trabaja planta (P) o administrativo (A): "))
     
 
-    if (consumo > 130000 ){
-        descuento = consumo * (15/100)
-        totalcondes  = consumo - descuento
+    if (Trabaja == "P" ){
+        horas = Number(prompt("¿Cantidad de horas trabajadas al día?: "))
+        pagahora = 20000
+        sueldo = horas * pagahora
 
-        console.log(`%cFormula ${consumo} * ${15} / ${100}`, `background: rebeccapurple; color: antiquewhite; font-family: cursive; padding: 2px; border-radius: 1px solid #925fc4; border-radius: 5px;`);
-        console.log(`Total de descuento: ${descuento}`);
+        console.log(`%cFormula ${horas} * ${pagahora} `, `background: rebeccapurple; color: antiquewhite; font-family: cursive; padding: 2px; border-radius: 1px solid #925fc4; border-radius: 5px;`);
 
-        console.log(`%cFormula ${consumo} - ${descuento}`, `background: rebeccapurple; color: antiquewhite; font-family: cursive; padding: 2px; border-radius: 1px solid #925fc4; border-radius: 5px;`);
-        console.log(`Total a pagar con descuento aplicado: ${totalcondes}`);
+        console.log(` Sueldo de trabajador de planta según la cantidad de horas introducidas: ${sueldo}`);
+
+    } else if(Trabaja == "A" ){
+        horas = Number(prompt("¿Cantidad de horas trabajadas al día?"))
+        pagahora = 10000
+        sueldo = horas * pagahora
+
+        console.log(`%cFormula ${horas} * ${pagahora} `, `background: rebeccapurple; color: antiquewhite; font-family: cursive; padding: 2px; border-radius: 1px solid #925fc4; border-radius: 5px;`);
+
+        console.log(`Sueldo de trabajador Administrativo según la cantidad de horas introducidas: ${sueldo}`);
+
     }
 })
